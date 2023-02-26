13e8fa33a48dbd7a7808fe0e75b2765d80991a55
sorodocosmin <61987774+sorodocosmin@users.noreply.github.com>
2023-02-26 18:24:48 +0200

    -added-homework

---
+++ Lab1-Week1/Homework/Main.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Homework homework1;
+        homework1 = new Homework();
+
+        homework1.doTheHomework(args);
+
+    }
+}
