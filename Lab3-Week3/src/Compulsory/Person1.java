ebe01bf1115e80139aa09082788610ecd2388e70
sorodocosmin <61987774+sorodocosmin@users.noreply.github.com>
2023-03-15 10:59:36 +0200

    git-ignore

--- Lab3-Week3/src/Compulsory/Person1.java
+++
@@ -1,9 +0,0 @@
-package Compulsory;
-
-public abstract class Person1 implements Comparable<Person1>, Node1{
-    protected String name;
-
-    @Override
-    public abstract int compareTo(Person1 person);
-
-}
--- Lab3-Week3/src/Homework/Node.java
+++
@@ -1,9 +0,0 @@
-package Homework;
-
-import java.util.Map;
-
-public interface Node {
-    public String getName();
-    public void addRelation(Node node, String typeOfRelation);
-    public Map<Node,String> getRelations();
-}
